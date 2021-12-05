4a80f1139e4a62c04021fb513d3fa11d872daf4e
KIRTIRAMASAMY <bkalaivani80@gmail.com>
2021-12-05 03:59:21 +0000

    mini project 4

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,3 +24,27 @@
     }
 </style>
 
+<script>
+    import Color from '$lib/Color.svelte';
+    let colorName = 'Light Green';
+</script>
+
+<div>
+    <h1 class="text-primary">Play with Colors</h1>
+</div>
+
+<div>
+    <input bind:value={colorName} />
+    <Color color={colorName} />
+</div>
+
+<style>
+    input {
+   	 padding: 0.5em;
+   	 text-align: center;
+    }
+    div {
+   	 text-align: center;
+   	 margin: 3em auto;
+    }
+</style>
